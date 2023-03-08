d3fb338015b71bf6d87b4b677b78c4cedb0a96d5
Sitael <Sitael@dummymail.com>
2023-03-08 18:47:52 +0300

    test imgui

--- content/baboon_editor/init.lua
+++ content/baboon_editor/init.lua
@@ -1,7 +1,25 @@
 local FULLPATH = ...
+local imgui = require("imgui")
+imgui.SetReturnValueLast(false)
+
+local function Update() imgui.NewFrame() end
+
+local function Draw()
+    if imgui.BeginMainMenuBar() then
+        if imgui.BeginMenu("File") then
+            imgui.MenuItem("Test")
+            imgui.EndMenu()
+        end
+        imgui.EndMainMenuBar()
+    end
+    imgui.Text("Hello, world!");
+    imgui.Render();
+end
+
 local function Init()
     local mod = SetupModule(FULLPATH)
-    local imgui = require("imgui")
+    mod.Update = Update
+    mod.Draw = Draw
     -- print(MODNAME)
     ---должен получать реквесты на регистрацию ui от модулей
     return mod
