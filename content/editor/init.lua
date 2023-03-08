5baeb873c379ec69c1af34669d140e6542886020
Sitael <Sitael@dummymail.com>
2023-03-08 12:27:40 +0300

    fix imgui path

--- content/editor/init.lua
+++ content/editor/init.lua
@@ -1,7 +1,7 @@
 local PATH = (...):gsub('%.[^%.]+$', '')
 local function Init()
     local mod = {}
-    local imgui = require(PATH .. "imgui")
+    local imgui = require(PATH .. "/imgui")
     return mod
 end
 
